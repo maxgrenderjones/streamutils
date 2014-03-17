cd50c9b69f6654090970157cdcc3c8c6fbe00772
Max Grender-Jones <maxgrenderjones@gmail.com>
2014-03-17 23:35:15 +0000

    Fix for weird py.test bug: https://bitbucket.org/hpk42/py-trunk/issue/116/importmismatcherror-or-importerror-encountered-running-pytest-doctest-modules-with-a-package

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ version=sys.version_info
 class PyTest(TestCommand):
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = '--doctest-modules --cov src --cov-report term-missing --doctest-glob=*.rst --ignore setup.py --ignore docs/conf.py'.split()
+        self.test_args = '--doctest-modules src/streamutils --cov src --cov-report term-missing --doctest-glob=*.rst --ignore setup.py --ignore docs/conf.py'.split()
         self.test_suite = True
     def run_tests(self):
         #import here, cause outside the eggs aren't loaded
